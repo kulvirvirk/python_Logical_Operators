8f9f8acbee5412778832dd3eb7e854ded63888b5
kulvirvirk <kvirkdev@gmail.com>
2020-02-20 01:23:20 +0000

    added example of '>' operator

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 # 1. print ascii value of 'a' and 'A'
+# 2. try > operator
+# 3. try < operator
+# 2. try == operator
+# 2. try >= operator
+# 2. try <= operator
 
+# 1. print ascii value of 'a' and 'A'
 print(ord('a'))
 print(ord('A'))
+print('--------------******--------------\n')
+
+# 2. try > operator
+num1 = 5
+num2 = 2
+print('num1: ' + str(num1))
+print('num2: ' + str(num2))
+if(num1 > num2):
+  print('num1 is greater!')
+else: print('num1 is not greater')
+print('--------------******--------------\n')
+
+
