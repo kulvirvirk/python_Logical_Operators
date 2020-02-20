1277dfc306baf2c7cd17fdc41c944b047c24e861
kulvirvirk <kvirkdev@gmail.com>
2020-02-20 01:26:52 +0000

    added code example of '==' operator

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # 1. print ascii value of 'a' and 'A'
 # 2. try > operator
 # 3. try < operator
-# 2. try == operator
-# 2. try >= operator
-# 2. try <= operator
+# 4. try == operator
+# 5. try >= operator
+# 6. try <= operator
 
 # 1. print ascii value of 'a' and 'A'
 print(ord('a'))
@@ -26,4 +26,12 @@ print('num2: ' + str(num2))
 if(num1 < num2):
   print('num1 is smaller!')
 else: print('num1 is not smaller')
+print('--------------******--------------\n')
+
+# 4. try == operator
+print('num1: ' + str(num1))
+print('num2: ' + str(num2))
+if(num1 == num2):
+  print('num1 is equal to num2!')
+else: print('num1 is not equal to num2')
 print('--------------******--------------\n')
