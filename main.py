56b67aa92d92b6cc43df465a1cb4f7277f3459a2
kulvirvirk <kvirkdev@gmail.com>
2020-02-20 01:29:58 +0000

    added code example of '>=' operator

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # 1. print ascii value of 'a' and 'A'
-# 2. try > operator
-# 3. try < operator
-# 4. try == operator
-# 5. try >= operator
-# 6. try <= operator
+# 2. try > operator; 'greater than'
+# 3. try < operator; 'smaller than'
+# 4. try == operator; 'equal to'
+# 5. try >= operator; 'greater than or equal to'
+# 6. try <= operator; 'smaller than or equal to'
 
 # 1. print ascii value of 'a' and 'A'
 print(ord('a'))
@@ -34,4 +34,14 @@ print('num2: ' + str(num2))
 if(num1 == num2):
   print('num1 is equal to num2!')
 else: print('num1 is not equal to num2')
+print('--------------******--------------\n')
+
+# 5. try >= operator
+num1 = 5
+num2 = 2
+print('num1: ' + str(num1))
+print('num2: ' + str(num2))
+if(num1 >= num2):
+  print('num1 is greater!')
+else: print('num1 is not greater')
 print('--------------******--------------\n')
